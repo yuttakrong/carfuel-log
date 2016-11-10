10433460a7b531072aa5a0f53a75f5963818c8aa
yuttakrong junsom <yuttakrong.j@gmail.com>
2016-11-10 14:31:39 +0700

    commit carfuel.data

--- CarFuel.Data/CarFuelDb.cs
+++ CarFuel.Data/CarFuelDb.cs
@@ -5,8 +5,14 @@ using System.Text;
 using System.Threading.Tasks;
 
 using System.Data.Entity; //For DbContext
+using CarFuel.Models.Facts;//using CarFuel.Models;
 
 namespace CarFuel.Data {
+
     public class CarFuelDb: DbContext {
+
+        public DbSet<Car> Cars { get; set; }
+
     }
+
 }
---
+++ CarFuel.Data/CarRepositor.cs
@@ -0,0 +1,16 @@
+﻿using CarFuel.Models.Facts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data.Entity;
+
+namespace CarFuel.Data {
+    public class CarRepositor : RepositoryBase<Car> {
+        public CarRepositor(DbContext context) : base(context) {
+
+        }
+    }
+}
